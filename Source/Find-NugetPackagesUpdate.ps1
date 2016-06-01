398b1bb07e2016b4845dca3eee2dfff37751d084
TraGicCode <michael@tragiccode.com>
2016-06-01 06:38:44 -0500

    More Tests

--- Source/Find-NugetPackagesUpdate.ps1
+++ Source/Find-NugetPackagesUpdate.ps1
@@ -15,7 +15,15 @@ Function Find-NugetPackagesUpdate
     [CmdletBinding()]
     [OutputType([PSCustomObject])]
     Param(
-        [ValidateNotNullOrEmpty()]
+        [ValidatePattern('[\\?]packages.config')]
+        [ValidateScript({
+            If (Test-Path $_) {
+                $True
+            }
+            else {
+                Throw "$_ is not a valid file located on the filesystem."
+            }
+        })]
         [Parameter(Mandatory=$True)]
         [String]
         $Path,
@@ -24,17 +32,17 @@ Function Find-NugetPackagesUpdate
         $ShowPreRelease
     )
     # Invoke-RestMethod -Method Get -Uri https://api.nuget.org/v3/index.json
-    $results = @()
-    $packages = Select-Xml -Path $Path -XPath "/packages/package" | Select-Object -ExpandProperty Node
-    ForEach ($package in $packages)
-    {
-        $response = Invoke-RestMethod -Method Get -Uri "https://api-v2v3search-0.nuget.org/query?q=$($package.id)&skip=0&take=1&prerelease=$(if ($ShowPreRelease.IsPresent) { $True } else { $False })&supportedFramework=.NETFramework,Version=v4.5"
-        $object = New-Object –TypeName PSObject
-        $object | Add-Member –MemberType NoteProperty –Name PackageId –Value $package.id
-        $object | Add-Member –MemberType NoteProperty –Name CurrentVersion –Value $package.version
-        $object | Add-Member -MemberType NoteProperty –Name NewVersion –Value $response[0].data.version
-        $results += $object
-    }
-    return $results
+    # $results = @()
+    # $packages = Select-Xml -Path $Path -XPath "/packages/package" | Select-Object -ExpandProperty Node
+    # ForEach ($package in $packages)
+    # {
+    #     $response = Invoke-RestMethod -Method Get -Uri "https://api-v2v3search-0.nuget.org/query?q=$($package.id)&skip=0&take=1&prerelease=$(if ($ShowPreRelease.IsPresent) { $True } else { $False })&supportedFramework=.NETFramework,Version=v4.5"
+    #     $object = New-Object –TypeName PSObject
+    #     $object | Add-Member –MemberType NoteProperty –Name PackageId –Value $package.id
+    #     $object | Add-Member –MemberType NoteProperty –Name CurrentVersion –Value $package.version
+    #     $object | Add-Member -MemberType NoteProperty –Name NewVersion –Value $response[0].data.version
+    #     $results += $object
+    # }
+    # return $results
 }
 
--- Tests/Find-NugetPackagesUpdate.Tests.ps1
+++ Tests/Find-NugetPackagesUpdate.Tests.ps1
@@ -3,11 +3,23 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\..\Source\$sut"
 
 Describe "Find-NugetPackagesUpdate" {
-    
+
     Context "Parameter Validation" {
-        
         It "should require a path that is not null or an empty string" {
-            { Find-NugetPackagesUpdate -Path "" } | Should Throw "The argument is null or empty"
+            { Find-NugetPackagesUpdate -Path "" } | Should Throw
+        }
+
+        It "should require a path that ends with a packages.config file" {
+            { Find-NugetPackagesUpdate -Path "Xpackages.config" } | Should Throw
+        }
+        
+        It "should throw when the file doesn't exist on the filesystem" {
+            { Find-NugetPackagesUpdate -Path "C:\SomeFolder\packages.config" } | Should Throw "is not a valid file located on the filesystem."
+        }
+
+        It "should not throw when the file exists on the filesystem" {
+            Mock Test-Path -MockWith { $True }
+            { Find-NugetPackagesUpdate -Path "C:\SomeFolder\packages.config" } | Should Not Throw
         }
     }
 }
