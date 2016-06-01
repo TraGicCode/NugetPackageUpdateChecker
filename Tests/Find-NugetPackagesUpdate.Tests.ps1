b8762b2bda0146caf4b6407b71613175c12e9c41
TraGicCode <michael@tragiccode.com>
2016-06-01 07:11:25 -0500

    Now using TestDrive for checking file existance and errors

--- Tests/Find-NugetPackagesUpdate.Tests.ps1
+++ Tests/Find-NugetPackagesUpdate.Tests.ps1
@@ -2,6 +2,39 @@
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\..\Source\$sut"
 
+# Notes for me
+# ====================================================
+# Mocks scope to only describe and context blocks
+
+# TestDrive
+# ====================================================
+# It is scope to describe blocks!
+# Is 'virtual file system' just for tests.  Its implemented as a PSDrive
+# When you enter a describe block you get a BRAND NEW virtual test file system that is just for your test.
+# You can
+# 1.) Write Files to it
+# 2.) Read files from it
+# 3.) Change Files from it
+# Doesn't affect the files on your machine
+# Doesn't depend on the users personal file system
+
+# Under the hood its creating a temporary drive in $env:Temp
+# $TestDrive contains absolute path to the testdrive
+# NOTE: Should be $env:Temp
+# Accessed like so TestDrive:\
+
+# Example
+# ====================================================
+# Describe {
+#   New-Item -Type File -Path TestDrive:\Describe.txt
+
+#   Context {
+#      "Hello" | Add-Content TestDrive:\Describe.txt
+#      New-Item -Type File -Path TestDrive:\Context.txt
+#   }
+# }
+
+
 Describe "Find-NugetPackagesUpdate" {
 
     Context "Parameter Validation" {
@@ -14,17 +47,24 @@ Describe "Find-NugetPackagesUpdate" {
         }
         
         It "should throw when the file doesn't exist on the filesystem" {
-            { Find-NugetPackagesUpdate -Path "C:\SomeFolder\packages.config" } | Should Throw "is not a valid file located on the filesystem."
+            { Find-NugetPackagesUpdate -Path "TestDrive:\packages.config" } | Should Throw "is not a valid file located on the filesystem."
         }
 
         It "should not throw when the file exists on the filesystem" {
-            Mock Test-Path -MockWith { $True }
-            { Find-NugetPackagesUpdate -Path "C:\SomeFolder\packages.config" } | Should Not Throw
+            # Arrange
+            New-Item -Type File -Path TestDrive:\packages.config
+            { Find-NugetPackagesUpdate -Path "TestDrive:\packages.config" } | Should Not Throw
+            # Assert
+            Remove-Item -Path TestDrive:\packages.config
         }
     }
     
     Context "Method Body" {
         It "should return an object of type array" {
+            { Find-NugetPackagesUpdate -Path "packages.config" } | Should BeOfType [PSCustomObject]
+        }
+        
+        It "should read a packages.config file's packages" {
             { Find-NugetPackagesUpdate -Path "packages.config" } | Should BeOfType [PSCustomObject]
         }
     }
